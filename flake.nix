7899b973f969f8a7f889a0b8edb02fc6bf410a24
zeropt <ferr0fluidmann@gmail.com>
2026-07-23 09:01:34 -0700

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "zigdragon flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+  in {
+    devShells = forAllSystems (system:
+    let
+      pkgs = pkgsFor.${system};
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [ zig_0_16 ];
+
+        shellHook = ''
+          echo "Using zigdragon devShell!"
+          echo "Zig version: $(zig version)"
+          echo ""
+        '';
+      };
+    });
+
+    packages = forAllSystems (system:
+    let
+      pkgs = pkgsFor.${system};
+    in {
+      default = pkgs.stdenv.mkDerivation {
+        name = "zigdragon";
+        src = ./.;
+        nativeBuildInputs = with pkgs; [ zig_0_16 ];
+
+        buildPhase = ''
+          zig build-exe zigdragon.zig \
+          -O ReleaseSmall \
+          -fstrip \
+          -fsingle-threaded \
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp zigdragon $out/bin
+        '';
+      };
+    });
+  };
+}
